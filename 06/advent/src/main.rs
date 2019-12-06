a555d67dcd689396c936238e30b1683b1b77302f
Andrew Weiss <wvvwwvw@gmail.com>
2019-12-05 20:29:58 -0800

    day 6 setup

---
+++ 06/advent/src/main.rs
@@ -0,0 +1,12 @@
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    let input = include_str!("../input.txt");
+    let mut prog = Vec::new();
+    for line in input.lines() {
+        for part in line.split(',') {
+            prog.push(part.parse::<isize>().unwrap());
+        }
+    }
+}
