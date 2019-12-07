d7ab5c5edce53ca389cda29ea93342fa90b9831f
Andrew Weiss <wvvwwvw@gmail.com>
2019-12-06 20:50:26 -0800

    day 7 setup

---
+++ 07/advent/src/main.rs
@@ -0,0 +1,11 @@
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet};
+use std::collections::VecDeque;
+
+fn main() {
+    let input = include_str!("../input.txt");
+    let mut graph = HashMap::new();
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split(')').collect();
+    }
+}
