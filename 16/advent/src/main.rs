f6f139b8fc66f0781f74c26f0660598b9967160a
Andrew Weiss <wvvwwvw@gmail.com>
2019-12-15 22:11:50 -0800

    day 16 part 2

--- 16/advent/src/main.rs
+++ 16/advent/src/main.rs
@@ -9,37 +9,37 @@ fn main() {
         data.push(c.to_digit(10).unwrap() as i32);
     }
 
-    for _ in 0..100 {
-        data = run(data);
+    let mut offset = 0;
+    for i in 0..7 {
+        offset = offset*10 + data[i];
     }
-    println!("{:?}", &data[0..8]);
+    let orig_offset = offset as usize;
+    let x = orig_offset / data.len();
+    let offset = x * data.len();
+
+    let mut real = Vec::with_capacity((10000-x+1)*data.len());
+    for _ in x..10000 {
+        real.append(&mut data.clone());
+    }
+
+    for _ in 0..100 {
+        real = run(real);
+    }
+    println!("{:?}", &real[(orig_offset - offset)..(orig_offset - offset + 8)]);
 }
 
 fn run(input: Vec<i32>) -> Vec<i32> {
     let mut result = vec![0; input.len()];
 
-    for i in 0..result.len() {
-        for (j, n) in input.iter().enumerate() {
-            result[i] += *n * factor(i as i32, j as i32);
-        }
-        result[i] = (result[i].abs()) % 10;
+    for n in input.iter() {
+        result[0] += *n;
     }
+
+    for i in 1..result.len() {
+        result[i] = result[i-1] - input[i-1];
+        result[i-1] = (result[i-1].abs()) % 10;
+    }
+    let l = result.len() - 1;
+    result[l] = (result[l].abs()) % 10;
     result
-}
-
-fn factor(i: i32, j: i32) -> i32 {
-     let cycle = (i + 1) * 4;
-
-     let pos = (j + 1) % cycle;
-
-     if pos < (i + 1) {
-         return 0;
-     }
-     if pos < 2*(i+1) {
-         return 1;
-     }
-     if pos < 3*(i+1) {
-         return 0;
-     }
-     return -1;
 }
