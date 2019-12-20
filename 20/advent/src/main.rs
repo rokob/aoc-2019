bcff251c8b55f9545bdc368a7c6ac6ac7d03db13
Andrew Weiss <wvvwwvw@gmail.com>
2019-12-20 00:12:17 -0800

    day 20 part 2

--- 20/advent/src/main.rs
+++ 20/advent/src/main.rs
@@ -7,7 +7,8 @@ enum Tile {
     Floor,
     Start,
     End,
-    Portal(usize),
+    InnerPortal(usize),
+    OuterPortal(usize),
 }
 
 fn main() {
@@ -19,8 +20,16 @@ fn main() {
     let mut portal_data = Vec::new();
     let mut start = (0, 0);
     let mut end = (0, 0);
+    let mut max_row = 0;
+    let mut max_col = 0;
     for (row, line) in input.lines().enumerate() {
+        if row > max_row {
+            max_row = row;
+        }
         for (col, c) in line.chars().enumerate() {
+            if col > max_col {
+                max_col = col;
+            }
             match c {
                 '#' => {
                     grid.insert((row, col), Tile::Wall);
@@ -78,36 +87,36 @@ fn main() {
 
     for (i, ((r0, c0), (r1, c1))) in portal_data.iter().enumerate() {
         if let Some(Tile::Floor) = grid.get(&(*r0 + 1, *c0)) {
-            grid.insert((*r0 + 1, *c0), Tile::Portal(i));
+            grid.insert((*r0 + 1, *c0), if *r0 == 1 { Tile::OuterPortal(i) } else { Tile::InnerPortal(i) });
         }
         if *r0 > 1 {
             if let Some(Tile::Floor) = grid.get(&(*r0 - 2, *c0)) {
-                grid.insert((*r0 - 2, *c0), Tile::Portal(i));
+                grid.insert((*r0 - 2, *c0), if *r0 == max_row { Tile::OuterPortal(i) } else { Tile::InnerPortal(i) });
             }
         }
         if let Some(Tile::Floor) = grid.get(&(*r0, *c0 + 1)) {
-            grid.insert((*r0, *c0 + 1), Tile::Portal(i));
+            grid.insert((*r0, *c0 + 1), if *c0 == 1 { Tile::OuterPortal(i) } else { Tile::InnerPortal(i) });
         }
         if *c0 > 1 {
             if let Some(Tile::Floor) = grid.get(&(*r0, *c0 - 2)) {
-                grid.insert((*r0, *c0 - 2), Tile::Portal(i));
+                grid.insert((*r0, *c0 - 2), if *c0 == max_col { Tile::OuterPortal(i) } else { Tile::InnerPortal(i) });
             }
         }
 
         if let Some(Tile::Floor) = grid.get(&(*r1 + 1, *c1)) {
-            grid.insert((*r1 + 1, *c1), Tile::Portal(i));
+            grid.insert((*r1 + 1, *c1), if *r1 == 1 { Tile::OuterPortal(i) } else { Tile::InnerPortal(i) });
         }
         if *r1 > 1 {
             if let Some(Tile::Floor) = grid.get(&(*r1 - 2, *c1)) {
-                grid.insert((*r1 - 2, *c1), Tile::Portal(i));
+                grid.insert((*r1 - 2, *c1), if *r1 == max_row { Tile::OuterPortal(i) } else { Tile::InnerPortal(i) });
             }
         }
         if let Some(Tile::Floor) = grid.get(&(*r1, *c1 + 1)) {
-            grid.insert((*r1, *c1 + 1), Tile::Portal(i));
+            grid.insert((*r1, *c1 + 1), if *c1 == 1 { Tile::OuterPortal(i) } else { Tile::InnerPortal(i) });
         }
         if *c1 > 1 {
             if let Some(Tile::Floor) = grid.get(&(*r1, *c1 - 2)) {
-                grid.insert((*r1, *c1 - 2), Tile::Portal(i));
+                grid.insert((*r1, *c1 - 2), if *c1 == max_col { Tile::OuterPortal(i) } else { Tile::InnerPortal(i) });
             }
         }
     }
@@ -148,25 +157,28 @@ fn main() {
         }
     }
 
-    let mut real_start = (0, 0);
-    let mut real_end = (0, 0);
+    let mut real_start = (0, 0, 0);
+    let mut real_end = (0, 0, 0);
 
     for ((row, col), tile) in grid.iter() {
         if *tile == Tile::Start {
-            real_start = (*row, *col);
+            real_start = (*row, *col, 0);
         }
         if *tile == Tile::End {
-            real_end = (*row, *col);
+            real_end = (*row, *col, 0);
         }
     }
 
     println!("start={:?}, end={:?}", real_start, real_end);
 
-    let mut real_portals = vec![vec![];portal_data.len()];
+    let mut real_portals = vec![vec![(0,0),(0,0)];portal_data.len()];
     for ((r, c), tile) in grid.iter() {
         match *tile {
-            Tile::Portal(i) => {
-                real_portals[i].push((*r, *c));
+            Tile::OuterPortal(i) => {
+                real_portals[i][0] = (*r, *c);
+            }
+            Tile::InnerPortal(i) => {
+                real_portals[i][1] = (*r, *c);
             }
             _ => {}
         }
@@ -181,58 +193,62 @@ fn main() {
             break;
         }
         let current = queue.pop_front().unwrap();
-        let tile = grid.get(&current).unwrap();
-        if *tile == Tile::End {
+        let tile = grid.get(&(current.0, current.1)).unwrap();
+        if *tile == Tile::End && current.2 == 0 {
             break;
         }
         if current.0 > 0 {
             if let Some(t) = grid.get(&(current.0 - 1, current.1)) {
-                if *t != Tile::Wall {
-                    if !path.contains_key(&(current.0 - 1, current.1)) {
-                        path.insert((current.0 - 1, current.1), current);
-                        queue.push_back((current.0 - 1, current.1));
+                if !(*t == Tile::Wall || (*t == Tile::Start && current.2 > 0) || (*t == Tile::End && current.2 > 0)) {
+                    if !path.contains_key(&(current.0 - 1, current.1, current.2)) {
+                        path.insert((current.0 - 1, current.1, current.2), current);
+                        queue.push_back((current.0 - 1, current.1, current.2));
                     }
                 }
             }
         }
         if let Some(t) = grid.get(&(current.0 + 1, current.1)) {
-            if *t != Tile::Wall {
-                if !path.contains_key(&(current.0 + 1, current.1)) {
-                    path.insert((current.0 + 1, current.1), current);
-                    queue.push_back((current.0 + 1, current.1));
+            if !(*t == Tile::Wall || (*t == Tile::Start && current.2 > 0) || (*t == Tile::End && current.2 > 0)) {
+                if !path.contains_key(&(current.0 + 1, current.1, current.2)) {
+                    path.insert((current.0 + 1, current.1, current.2), current);
+                    queue.push_back((current.0 + 1, current.1, current.2));
                 }
             }
         }
         if current.1 > 0 {
             if let Some(t) = grid.get(&(current.0, current.1 - 1)) {
-                if *t != Tile::Wall {
-                    if !path.contains_key(&(current.0, current.1 - 1)) {
-                        path.insert((current.0, current.1 - 1), current);
-                        queue.push_back((current.0, current.1 - 1));
+                if !(*t == Tile::Wall || (*t == Tile::Start && current.2 > 0) || (*t == Tile::End && current.2 > 0)) {
+                    if !path.contains_key(&(current.0, current.1 - 1, current.2)) {
+                        path.insert((current.0, current.1 - 1, current.2), current);
+                        queue.push_back((current.0, current.1 - 1, current.2));
                     }
                 }
             }
         }
         if let Some(t) = grid.get(&(current.0, current.1 + 1)) {
-            if *t != Tile::Wall {
-                if !path.contains_key(&(current.0, current.1 + 1)) {
-                    path.insert((current.0, current.1 + 1), current);
-                    queue.push_back((current.0, current.1 + 1));
+            if !(*t == Tile::Wall || (*t == Tile::Start && current.2 > 0) || (*t == Tile::End && current.2 > 0)) {
+                if !path.contains_key(&(current.0, current.1 + 1, current.2)) {
+                    path.insert((current.0, current.1 + 1, current.2), current);
+                    queue.push_back((current.0, current.1 + 1, current.2));
                 }
             }
         }
         match tile {
-            Tile::Portal(i) => {
+            Tile::OuterPortal(i) => {
+                if current.2 > 0 {
                 let data = &real_portals[*i];
-                if current.0 == data[0].0 && current.1 == data[0].1 {
-                    if !path.contains_key(&data[1]) {
-                        path.insert(data[1], current);
-                        queue.push_back(data[1]);
+                    if !path.contains_key(&(data[1].0, data[1].1, current.2 - 1)) {
+                        path.insert((data[1].0, data[1].1, current.2 - 1), current);
+                        queue.push_back((data[1].0, data[1].1, current.2 - 1));
                     }
-                } else {
-                    if !path.contains_key(&data[0]) {
-                        path.insert(data[0], current);
-                        queue.push_back(data[0]);
+                }
+            },
+            Tile::InnerPortal(i) => {
+                if current.2 < 50 {
+                let data = &real_portals[*i];
+                    if !path.contains_key(&(data[0].0, data[0].1, current.2 + 1)) {
+                        path.insert((data[0].0, data[0].1, current.2 + 1), current);
+                        queue.push_back((data[0].0, data[0].1, current.2 + 1));
                     }
                 }
             },
