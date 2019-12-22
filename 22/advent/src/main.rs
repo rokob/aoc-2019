e43d1544b36eb80ebfc83dbf341278c9ba2dffb6
Andrew Weiss <wvvwwvw@gmail.com>
2019-12-21 21:23:17 -0800

    part 1 day 22

--- 22/advent/src/main.rs
+++ 22/advent/src/main.rs
@@ -2,231 +2,76 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-enum State {
-    Input,
-    Output,
-    Running,
-    Halt,
+enum Step {
+    Deal,
+    Incr(usize),
+    Cut(isize),
 }
+
+const N: usize = 10007;
 
 fn main() {
     let input = include_str!("../input.txt");
-    let mut data = Vec::new();
+    let mut steps = Vec::new();
     for line in input.lines() {
-        for part in line.split(',') {
-            data.push(part.parse::<isize>().unwrap());
-        }
-    }
-
-        
-    let instr = "NOT C J\nNOT E T\nAND T J\nNOT G T\nAND T J\nNOT I T\nAND T J\nNOT A T\nOR T J\nNOT B T\nNOT T T\nAND F T\nAND H T\nOR T J\nAND D J\nRUN\n";
-    let instr = instr.chars().map(|c| c as u32).collect::<Vec<_>>();
-    let mut instr_idx = 0;
-    let mut last = 0;
-    let mut s = String::new();
-
-    let mut prog = Program::new(data);
-    prog.start();
-    loop {
-        let state = prog.state();
-        match state {
-            State::Running => prog.run(),
-            State::Input => {
-                prog.input(instr[instr_idx] as isize);
-                instr_idx += 1;
-            }
-            State::Output => {
-                if let Some(output) = prog.output()  {
-                    match output {
-                        10 => {
-                            s.push('\n');
-                            if last == 10 {
-                                print!("{}", s);
-                                s.clear();
-                            }
-                        },
-                        c if c < 127 => {
-                            let c = char::from(output as u8);
-                            s.push(c);
-                        },
-                        _ => println!("\nResult: {}", output),
-                    }
-                    last = output;
-            }
-            }
-            State::Halt => {
-                break;
-            }
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-struct Program {
-    ip: usize,
-    rb: isize,
-    data: Vec<isize>,
-    needs_input: bool,
-    has_output: bool,
-    running: bool,
-}
-
-impl Program {
-    fn new(data: Vec<isize>) -> Self {
-        Program {
-            ip: 0,
-            rb: 0,
-            data,
-            needs_input: false,
-            has_output: false,
-            running: true,
-        }
-    }
-
-    fn start(&mut self) {
-        self.run();
-    }
-
-    fn state(&self) -> State {
-        if !self.running {
-            return State::Halt;
-        }
-        if self.needs_input {
-            return State::Input;
-        }
-        if self.has_output {
-            return State::Output;
-        }
-        State::Running
-    }
-
-    fn input(&mut self, in_: isize) {
-        if !self.running {
-            return;
-        }
-        let (_inst, a, _b, _c) = parse(self.data[self.ip]);
-        let xi = self.index(a, 1);
-        self.data[xi] = in_;
-        self.ip += 2;
-        self.needs_input = false;
-    }
-
-    fn output(&mut self) -> Option<isize> {
-        if !self.has_output || !self.running {
-            return None;
-        }
-        let (_inst, a, _b, _c) = parse(self.data[self.ip]);
-        let xi = self.index(a, 1);
-        let x = self.data[xi];
-        self.has_output = false;
-        self.ip += 2;
-        Some(x)
-    }
-
-    fn run(&mut self) {
-        if !self.running {
-            return;
-        }
-
-        let (inst, a, b, c) = parse(self.data[self.ip]);
-        if inst == 99 {
-            self.running = false;
-            return;
-        }
-        let xi = self.index(a, 1);
-        let x = self.data[xi];
-        match inst {
-            1 => {
-                let y = self.read(b, 2);
-                let z = self.index(c, 3);
-                self.data[z] = x + y;
-                self.ip += 4;
-            }
-            2 => {
-                let y = self.read(b, 2);
-                let z = self.index(c, 3);
-                self.data[z] = x * y;
-                self.ip += 4;
-            }
-            3 => {
-                self.needs_input = true;
-                return;
-            }
-            4 => {
-                self.has_output = true;
-                return;
-            }
-            5 => {
-                let y = self.read(b, 2);
-                if x != 0 {
-                    self.ip = y as usize;
-                } else {
-                    self.ip += 3;
+        let parts = line.split_whitespace().collect::<Vec<_>>();
+        match parts[0] {
+            "deal" => {
+                match parts[1] {
+                    "with" => {
+                        steps.push(Step::Incr(parts[3].parse::<usize>().unwrap()));
+                    },
+                    "into" => {
+                        steps.push(Step::Deal);
+                    },
+                    _ => panic!("bad input: {:?}", line),
                 }
             }
-            6 => {
-                let y = self.read(b, 2);
-                if x == 0 {
-                    self.ip = y as usize;
-                } else {
-                    self.ip += 3;
-                }
-            }
-            7 | 8 => {
-                let y = self.read(b, 2);
-                let z = self.index(c, 3);
-                if (x < y && inst == 7) || (x == y && inst == 8) {
-                    self.data[z] = 1;
-                } else {
-                    self.data[z] = 0;
-                }
-                self.ip += 4;
-            }
-            9 => {
-                self.ip += 2;
-                self.rb += x;
-            }
-            _ => panic!("unk::{}", inst),
+            "cut" => {
+                steps.push(Step::Cut(parts[1].parse::<isize>().unwrap()));
+            },
+            _ => panic!("bad input: {:?}", line),
         }
     }
 
-    #[inline]
-    fn maybe_extend(&mut self, idx: usize) {
-        if idx < self.data.len() {
-            return;
-        }
-        self.data.resize(self.data.len() * 2, 0);
-    }
+    let mut deck = (0..N).collect::<Vec<usize>>();
 
-    #[inline]
-    fn index(&mut self, mode: isize, offset: usize) -> usize {
-        if mode == 0 {
-            let i = self.data[self.ip + offset] as usize;
-            self.maybe_extend(i as usize);
-            i
-        } else if mode == 2 {
-            let i = (self.rb + self.data[self.ip + offset]) as usize;
-            self.maybe_extend(i as usize);
-            i
-        } else {
-            self.ip + offset
+    for step in steps.iter() {
+        take_step(step, &mut deck);
+    }
+    let mut result = 0;
+    let goal = 2019;
+    for (i, elem) in deck.iter().enumerate() {
+        if *elem == goal {
+            result =  i;
+            break;
         }
     }
-
-    #[inline]
-    fn read(&mut self, mode: isize, offset: usize) -> isize {
-        let i = self.index(mode, offset);
-        self.data[i]
-    }
+    println!("{}", result);
 }
 
-fn parse(mut inst: isize) -> (isize, isize, isize, isize) {
-    let x = inst % 100;
-    inst /= 100;
-    let a = inst % 10;
-    inst /= 10;
-    let b = inst % 10;
-    inst /= 10;
-    let c = inst % 10;
-    (x, a, b, c)
+fn take_step(step: &Step, deck: &mut Vec<usize>) {
+    match step {
+        Step::Deal => {
+            deck.reverse();
+        },
+        Step::Incr(i) => {
+            let mut result = vec![0; N];
+            let mut idx = 0;
+            for elem in deck.iter() {
+                result[idx] = *elem;
+                idx = (idx + i) % deck.len();
+            }
+            for (i, elem) in result.into_iter().enumerate() {
+                deck[i] = elem;
+            }
+        },
+        Step::Cut(i) => {
+            if *i > 0 {
+                deck.rotate_left(*i as usize);
+            } else {
+                deck.rotate_right((-*i) as usize);
+            }
+        }
+    }
 }
