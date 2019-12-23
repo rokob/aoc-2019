71351e608a4ac86f243972ff39243cc9a90d944b
Andrew Weiss <wvvwwvw@gmail.com>
2019-12-22 20:55:38 -0800

    day 18 more

--- 18/advent/src/main.rs
+++ 18/advent/src/main.rs
@@ -8,6 +8,19 @@ enum Token {
     Wall,
 }
 
+fn mark_seen(i: u32, key: char) -> u32 {
+    let bit = key as u8 - 97u8;
+    i | (1 << bit)
+}
+
+fn is_seen(i: u32, key: char) -> bool {
+    let bit = key as u8 - 97u8;
+    i & (1 << bit) > 0
+}
+
+                      //abcdefghijklmnopqrstuvwxyz
+const ALL_KEYS: u32 = 0b11111111111111111111111111;
+
 fn main() {
     let input = include_str!("../input.txt");
     let mut tokens = HashMap::new();
@@ -15,11 +28,13 @@ fn main() {
     let mut doors = HashMap::new();
     let mut y_max = 0;
     let mut x_max = 0;
+    let mut start = (0, 0);
     for (y, line) in input.lines().enumerate() {
         for (x, c) in line.chars().enumerate() {
             match c {
                 '@' => {
                     tokens.insert((x,y), Token::Entrance);
+                    start = (x, y);
                 },
                 '.' => {
                 },
@@ -43,6 +58,7 @@ fn main() {
         y_max = if y > y_max { y } else { y_max };
     }
 
+    /*
     for y in 0..=y_max {
         for x in 0..=x_max {
             match tokens.get(&(x,y)) {
@@ -54,5 +70,50 @@ fn main() {
             }
         }
         println!("");
+    }
+    */
+    let result = shortest_path(&tokens, &keys, start);
+}
+
+fn shortest_path(tokens: &HashMap<(usize, usize), Token>, keys: &HashMap<Token, (usize, usize)>, start: (usize, usize, u32)) -> usize {
+    let mut queue = VecDeque::new();
+    let mut seen = HashSet::new();
+    queue.push_back(start);
+    seen.insert(start);
+    let dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+    while !queue.is_empty() {
+        let current = queue.pop_front().unwrap();
+
+        match tokens.get(&(current.0, current.1)) {
+            None => {// floor
+                for (row, col) in dirs.iter() {
+                    let pos = ((current.0 as isize + row) as usize, (current.1 as isize + col) as usize, current.2);
+                    if seen.insert(pos) {
+                        queue.push_back(pos);
+                    }
+                }
+            },
+            Some(Token::Wall) => {},
+            Some(Token::Entrance) => {},
+            Some(Token::Door(c)) => {
+                if is_seen(current.2, c) {
+                    for (row, col) in dirs.iter() {
+                        let pos = ((current.0 as isize + row) as usize, (current.1 as isize + col) as usize, current.2);
+                        if seen.insert(pos) {
+                            queue.push_back(pos);
+                        }
+                    }
+                }
+            },
+            Some(Token::Key(c)) => {
+                let new_keys = mark_seen(current.2, c);
+                for (row, col) in dirs.iter() {
+                    let pos = ((current.0 as isize + row) as usize, (current.1 as isize + col) as usize, new_keys);
+                    if seen.insert(pos) {
+                        queue.push_back(pos);
+                    }
+                }
+            },
+        }
     }
 }
